8fea8f6f60d22822c95963f66a7299b2e6ae82d9
André Santana <67551515+andreo-san@users.noreply.github.com>
2022-05-25 12:19:16 -0300

    Update script.js

--- script.js
+++ script.js
@@ -186,3 +186,26 @@ function salvarLocal(){
     window.localStorage.setItem("db_hist_entradas", JSON.stringify(historico_de_entradas));
     window.localStorage.setItem("db_hist_geral", JSON.stringify(historico_geral));
 }
+
+const ctx = document.getElementById('myChart').getContext('2d');
+    const myChart = new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            labels: [
+                'Assinaturas',
+                'Casa',
+                'Compras',
+                'Dívidas'
+              ],
+              datasets: [{
+                label: 'My First Dataset',
+                data: [300, 50, 100],
+                backgroundColor: [
+                  'rgb(255, 99, 132)',
+                  'rgb(54, 162, 235)',
+                  'rgb(255, 205, 86)'
+                ],
+                hoverOffset: 4
+              }]
+        }
+    });
